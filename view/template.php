cb04a3e6d466528187921a1b6fa20fc06209d203
JoseG <josegregorio.blaze@gmail.com>
2021-02-26 04:07:16 -0400

    se agrego comentario de prueba

--- view/template.php
+++ view/template.php
@@ -20,7 +20,7 @@ ob_start();
 <!DOCTYPE html>
 <html lang="en">
 <head>
-
+    <!-- prueba head J -->
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
